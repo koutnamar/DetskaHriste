8b24df0c091af55aad78c2d6df13862c44be183a
ZdenkaSl <z.slezakova@gmail.com>
2018-11-25 13:34:40 +0100

    commentDao

--- src/cz/czechitas/detskahriste/dao/CommentDao.java
+++ src/cz/czechitas/detskahriste/dao/CommentDao.java
@@ -1,5 +1,55 @@
 package cz.czechitas.detskahriste.dao;
 
-public class CommentDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import javax.sql.DataSource;
+
+import cz.czechitas.detskahriste.bean.Comment;
+
+public class CommentDao extends JdbcDao {
+	private static final String LOAD = "SELECT * FROM COMMENT WHERE idFkPlayCom = ?"; //naèíst všechny záznamy z tabulky comment
+	private static final String INSERT = "INSERT INTO COMMENT(user,text,date,idFkPlayCom) VALUES (?,?,?,?)";
+
+	public void save(Comment comment, Long idPlayground) {
+		DataSource ds = getDataSource();
+		try (Connection con = ds.getConnection();
+			PreparedStatement stmt = con.prepareStatement(INSERT))
+		{
+			stmt.setString(1, comment.getUser());
+			stmt.setString(2, comment.getText());
+			stmt.setTimestamp(3, java.sql.Timestamp.valueOf(comment.getDate()));
+			stmt.setLong(4, idPlayground);
+			stmt.executeUpdate();
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+
+	}
+
+	public ArrayList<Comment> load(Long idPlayground) {
+		ArrayList<Comment> list = new ArrayList<>(); //do toho seznamu uložím, co najdu
+		DataSource ds = getDataSource(); //získám pøipojení k DB
+		try (Connection con = ds.getConnection(); //pøipojení k DB
+			PreparedStatement stmt = con.prepareStatement(LOAD)) //javovská konstrukce, jak se provolává SELECT do DB 
+		{
+			ResultSet rs = stmt.executeQuery();	//vytáhnou se komentáøe patøící høišti
+			while (rs.next()) { //v rs jsou data, která potøebuju nasetovat do svých objektù
+				Comment com = new Comment();
+				com.setIdComment(rs.getLong("idComment"));
+				com.setUser(rs.getString("user"));
+				com.setText(rs.getString("text"));
+				com.setDate(rs.getTimestamp("date").toLocalDateTime());;
+				list.add(com);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
 }
