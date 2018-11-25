ff0e6e207ff4c1b871bba7dfb25e7b6652352204
marktn <koutna.mar@gmail.com>
2018-11-25 11:11:47 +0100

    added location

--- src/cz/czechitas/detskahriste/bean/Photo.java
+++ src/cz/czechitas/detskahriste/bean/Photo.java
@@ -5,4 +5,37 @@ public class Photo {
 	private String namePhoto;
 	private String path;
 	private Long idFkPlayPhoto;
+
+	public Long getIdPhoto() {
+		return idPhoto;
+	}
+
+	public void setIdPhoto(Long idPhoto) {
+		this.idPhoto = idPhoto;
+	}
+
+	public String getNamePhoto() {
+		return namePhoto;
+	}
+
+	public void setNamePhoto(String namePhoto) {
+		this.namePhoto = namePhoto;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Long getIdFkPlayPhoto() {
+		return idFkPlayPhoto;
+	}
+
+	public void setIdFkPlayPhoto(Long idFkPlayPhoto) {
+		this.idFkPlayPhoto = idFkPlayPhoto;
+	}
+
 }
