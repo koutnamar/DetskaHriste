a06a5571cdcc7f099e7212d336ffd3dc9084d0a5
marktn <koutna.mar@gmail.com>
2018-11-27 18:01:27 +0100

    vyhledavani city, street na indexu + playgroundDao

--- src/cz/czechitas/detskahriste/dao/PlaygroundDao.java
+++ src/cz/czechitas/detskahriste/dao/PlaygroundDao.java
@@ -15,6 +15,7 @@ public class PlaygroundDao extends JdbcDao {
 	private static final String LOAD = "SELECT * FROM playground WHERE idPlayground = ?"; // naèíst všechny záznamy z
 																							// tabulky playground
 	private static final String LOAD_ALL = "SELECT * FROM playground";
+	private static final String LOAD_ALL_SELECT = "select * from playground, location where playground.idPlayground=location.idFkPlayLoc and location.city like ? and location.street like ?";
 	private static final String INSERT = "INSERT INTO playground(open,traffic) VALUES (?,?)";
 	private final CommentDao commentDao = new CommentDao();
 	private final LocationDao locationDao = new LocationDao();
@@ -87,6 +88,42 @@ public class PlaygroundDao extends JdbcDao {
 		return list;
 	}
 
+	public ArrayList<Playground> loadCityStreet(String city, String street ) {
+		if (city == null) {
+			city = "";
+		}
+		if (street == null) {
+			street = "";
+		}
+		ArrayList<Playground> list = new ArrayList<Playground>(); 
+		DataSource ds = getDataSource(); 
+		try (Connection con = ds.getConnection(); 
+				PreparedStatement stmt = con.prepareStatement(LOAD_ALL_SELECT)) 
+		{
+			stmt.setString(1,city+"%");
+			stmt.setString(2, street+"%");
+			ResultSet rs = stmt.executeQuery(); 
+			while (rs.next()) { 
+				Playground playground = new Playground();
+				playground.setIdPlayground(rs.getLong("idPlayground"));
+				playground.setOpen(rs.getString("open"));
+				playground.setTraffic(rs.getString("traffic"));
+				list.add(playground);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		for (Playground playground : list) {
+			playground.setRating(ratingDao.load(playground.getIdPlayground()));
+			playground.setAverageRating(calculateAverage(playground.getRating()));
+			playground.setCommentList(commentDao.load(playground.getIdPlayground()));
+			playground.setLocation(locationDao.load(playground.getIdPlayground()));
+			playground.setPhotoList(photoDao.load(playground.getIdPlayground()));
+		}
+		return list;
+	}
+
+	
 	private Double calculateAverage(Rating rating) {
 		if (rating == null) {
 			return 0d;
